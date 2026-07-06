9db12b6e9eee1d0ff6859305e4fbe74c4c59ca37
ameycode88 <146335383+ameycode88@users.noreply.github.com>
2026-07-06 15:59:27 +0530

    Add data import documentation for staging tables
    Documented the data import process and the tables involved.

---
+++ sql/02_import_data.sql
@@ -0,0 +1,15 @@
+-- Data Import
+
+-- All CSV files were imported into their respective staging tables
+-- using pgAdmin Import/Export Wizard.
+
+-- Imported Tables:
+-- stg_customers
+-- stg_orders
+-- stg_order_items
+-- stg_order_payments
+-- stg_order_reviews
+-- stg_products
+-- stg_sellers
+-- stg_geolocation
+-- stg_product_category_name_translation
