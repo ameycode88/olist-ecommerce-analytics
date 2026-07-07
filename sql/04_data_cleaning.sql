ecac520d798844a42b859d8308c7dc99272d1dff
ameycode88 <146335383+ameycode88@users.noreply.github.com>
2026-07-07 12:48:57 +0530

    Created clean_products table with translated product categories
    Created a clean_products table to store translated product categories and relevant product details.

--- sql/04_data_cleaning.sql
+++ sql/04_data_cleaning.sql
@@ -1,1 +1,21 @@
+-- ==========================================
+-- Data Cleaning: Create clean_products Table
+-- Translate Portuguese Product Categories to English
+-- ==========================================
 
+CREATE TABLE clean_products AS
+SELECT
+p.product_id,
+COALESCE(
+t.product_category_name_english,
+'Unknown'
+) AS category,
+p.product_weight_g,
+p.product_length_cm,
+p.product_height_cm,
+p.product_width_cm
+FROM stg_products p
+LEFT JOIN stg_product_category_name_translation t
+ON p.product_category_name=t.product_category_name;
+
+select * from clean_products;
